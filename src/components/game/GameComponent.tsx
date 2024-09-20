c55824f7e01440c6c6c8bd6a3a7f4d66e9af79ed
shapl_xlbobtq <sergeshapluk@gmail.com>
2024-09-21 00:35:36 +0300

    add game

--- src/components/game/GameComponent.tsx
+++ src/components/game/GameComponent.tsx
@@ -1,46 +1,45 @@
-// import {ThemeInitializer} from "./contexts/ThemeContext";
-// import {Game} from "./components/Game";
+import {ThemeInitializer} from "./contexts/ThemeContext";
+import {Game} from "./components/Game";
 
-import MemoHandcuffIcon from "../svg/HandcuffIcon";
+// import MemoHandcuffIcon from "../svg/HandcuffIcon";
 
 const GameComponent = () => {
 
-    //
-    // return (
-    //     <ThemeInitializer>
-    //         <Game/>
-    //     </ThemeInitializer>
-    // );
+
     return (
-        <div style={{
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center"
-        }}>
-            <div style={{
-                color: "#3193F4",
-                fontFamily: "Onest",
-                fontSize: "29px",
-                fontWeight: "500",
-                lineHeight: "33px",
-                marginTop: 20
-            }}>Game
-            </div>
-            <div style={{
-                color: "#FFFFFF",
-                fontFamily: "Onest",
-                fontWeight: "400",
-                fontSize: "20px",
-                lineHeight: "22px",
-                marginTop: 104
-            }}>А game will
-                start soon
-            </div>
-            <MemoHandcuffIcon style={{position: "absolute", bottom: 0}}/>
-        </div>
+        <ThemeInitializer>
+            <Game/>
+        </ThemeInitializer>
+        // return (
+        //     <div style={{
+        //         width: "100%",
+        //         height: "100%",
+        //         display: "flex",
+        //         flexDirection: "column",
+        //         justifyContent: "center",
+        //         alignItems: "center"
+        //     }}>
+        //         <div style={{
+        //             color: "#3193F4",
+        //             fontFamily: "Onest",
+        //             fontSize: "29px",
+        //             fontWeight: "500",
+        //             lineHeight: "33px",
+        //             marginTop: 20
+        //         }}>Game
+        //         </div>
+        //         <div style={{
+        //             color: "#FFFFFF",
+        //             fontFamily: "Onest",
+        //             fontWeight: "400",
+        //             fontSize: "20px",
+        //             lineHeight: "22px",
+        //             marginTop: 104
+        //         }}>А game will
+        //             start soon
+        //         </div>
+        //         <MemoHandcuffIcon style={{position: "absolute", bottom: 0}}/>
+        //     </div>
     );
 };
 
